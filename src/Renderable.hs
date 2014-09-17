e96ad3a5467539ff6bc3e55d432f65ada214000e
Cerek Hillen <crockeo@gmail.com>
2014-09-17 14:19:25 -0400

    Added some utils regarding rendering.

---
+++ src/Renderable.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ExistentialQuantification #-}
+module Renderable where
+
+--------------------
+-- Global Imports --
+import Control.Applicative
+
+----------
+-- Code --
+
+-- | A typeclass to define that a type can be rendered. Should only be used to
+--   perform render calls.
+class Renderable a where
+  render :: a -> IO ()
+
+-- | A generic render type.
+data Render = forall a. Renderable a => Render a
+
+-- | A do-composable list of render calls.
+data RendersT a = RendersT [Render] a
+
+-- | The commonly used version of @'RendersT'@.
+type Renders = RendersT ()
+
+-- | Maps over the value contained in a @'RendersT'@.
+instance Functor RendersT where
+  fmap fn (RendersT l a) = RendersT l $ fn a
+
+-- | Applies composable functions to a @'RendersT'@. In the event that the
+--   function contains a list, it will be appended to the list in the applied
+--   value.
+instance Applicative RendersT where
+  pure a = RendersT [] a
+  (RendersT l' fn) <*> (RendersT l a) =
+    RendersT (l ++ l') $ fn a
+
+-- | The main sauce of the @'RendersT'@. This allows @'Render'@ calls to be
+--   composd through do-notation. So one can purely emulate IO-based rendering
+--   and then just render everything in the end.
+instance Monad RendersT where
+  return = pure
+  (RendersT l a) >>= fn =
+    let (RendersT l' a') = fn a in
+      RendersT (l ++ l') a'

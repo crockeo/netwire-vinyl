810746dadc0c38c6e0d0df9a937d297fdc7b7657
Cerek Hillen <crockeo@gmail.com>
2014-09-18 09:31:08 -0400

    Implemented the handing of GLFW in the main function.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,48 @@
 module Main where
 
+--------------------
+-- Global Imports --
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLFW as GLFW
+import Data.IORef
+
 ----------
 -- Code --
+
+-- | The callback for when the window should be closed.
+makeWindowCloseCallback :: IORef Bool -> WindowCloseCallback
+makeWindowCloseCallback closedRef = do
+  writeIORef closedRef True
+  return True
+
+-- | The callback for when the window is resized.
+makeWindowSizeCallback :: WindowSizeCallback
+makeWindowSizeCallback s@(Size w h) = do
+  viewport $= (Position 0 0, s)
+
+  matrixMode $= Projection
+  loadIdentity
+
+  ortho 0 (fromIntegral w) (fromIntegral h) 0 (-1) 1
+
+  matrixMode $= Modelview 0
 
 -- | The entry point to the program. Sets things up and passes it off to the
 --   Netwire network.
 main :: IO ()
-main = putStrLn "Nothing here."
+main = do
+  initialize
+  openWindow (Size 640 480) [DisplayRGBBits 8 8 8, DisplayAlphaBits 8, DisplayDepthBits 24] Window
+  windowTitle $= "netwire-vinyl"
+
+  closedRef <- newIORef False
+  windowCloseCallback $= do
+    writeIORef closedRef True
+    return True
+
+  windowCloseCallback $= makeWindowCloseCallback closedRef
+  windowSizeCallback  $= makeWindowSizeCallback
+
+  -- HOOK UP TO NETWORK
+
+  closeWindow

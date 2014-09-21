2593c9f1f9c01255297dc808c8723821fd168875
Crockeo <crockeo@gmail.com>
2014-09-21 00:01:28 -0400

    added loading the food assets.

--- src/Network.hs
+++ src/Network.hs
@@ -22,7 +22,11 @@ import World
 -- | Loading the project's assets.
 loadAssets :: AssetLoads
 loadAssets = do
+  loadTexture       "apple.png"
+  loadTexture       "cherry.png"
   loadTexture       "crate.png"
+  loadTexture       "lemon.png"
+  loadTexture       "mouse.png"
   loadShaderProgram "game2d"
 
 -- | The backend to running the network.

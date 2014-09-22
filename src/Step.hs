b614e0ff3163ace93df055f7b888645b73686ce3
Crockeo <crockeo@gmail.com>
2014-09-22 12:35:08 -0400

    Implemented the stepper system. Untested as of yet.

--- src/Step.hs
+++ src/Step.hs
@@ -4,6 +4,7 @@ module Step ( stepper
 
 --------------------
 -- Global Imports --
+import Prelude hiding ((.))
 import Control.Wire
 
 ----------
@@ -20,7 +21,34 @@ stepper target t =
                  then (Right a     , stepper target 0 )
                  else (Left  mempty, stepper target t')
 
+-- | A constant version of @'stepper'@ where it never blocks, but rather
+--   returns @'True'@ when @'stepper'@ ticks.
+constStepper :: (Fractional t, HasTime t s, Monoid e, Monad m) => t -> t -> Wire s e m a Bool
+constStepper target t  =  pure True . stepper target t
+                      <|> pure False
+
+-- | Stepping a wire if the input is true. Steps once upon calling the
+--   function, but only calls it again when provided with a @True@ input.
+--   Inhibits forever when an inhibition signal is generated.
+stepIf :: (Monoid s, Monoid e, Monad m) => Wire s e m a b -> Wire s e m (a, Bool) b
+stepIf initWire =
+  mkGen $ \ds (input, _) -> do
+    (b, wire') <- stepWire initWire ds $ Right input
+    case b of
+      Left  b' -> return (Left  b', inhibit b')
+      Right b' -> return (Right b', stepIf' b' wire')
+  where stepIf' :: (Monoid s, Monoid e, Monad m) => b -> Wire s e m a b -> Wire s e m (a, Bool) b
+        stepIf' buf wire =
+          mkGen $ \ds (input, step) -> do
+            if not step
+              then return (Right buf, stepIf' buf wire)
+              else do
+                (b, wire') <- stepWire wire ds $ Right input
+                case b of
+                  Left  b' -> return (Left  b', inhibit b')
+                  Right b' -> return (Right b', stepIf' b' wire')
+
 -- | Stepping an input wire periodically. The time between updates is specified
 --   exactly like in the @'stepper'@ function.
-periodicWire :: t -> Wire s e m a b -> Wire s e m a b
-periodicWire _ _ = undefined
+periodicWire :: (Fractional t, HasTime t s, Monoid e, Monad m) => t -> Wire s e m a b -> Wire s e m a b
+periodicWire target wire = stepIf wire . liftA2 (,) mkId (constStepper target 0)
